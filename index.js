4821c2eaee66a5adec610859fc44e043394bb8e7
Shane Tomlinson <stomlinson@mozilla.com>
2017-12-06 21:17:25 +0000

    Initial checkin

---
+++ index.js
@@ -0,0 +1,17 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+module.exports = function (source) {
+  return
+    `
+    var mustache = require("mustache");
+    module.exports = function () {
+      return function (view, partials) {
+        var src = ${JSON.stringify(source)}
+        return mustache.render(src, view, partials);
+      }
+    }();
+    `;
+};
+
